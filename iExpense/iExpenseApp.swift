7ed05fad0d625e36a2e2e34de4240ae2e8ec51e5
Fernando Gomez <redchameleonapps@gmail.com>
2023-02-06 23:51:10 -0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Fernando Gomez on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
