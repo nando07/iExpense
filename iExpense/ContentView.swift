f8aee9ab643bc756b4279a88eeab7a0b20959bb1
Fernando Gomez <redchameleonapps@gmail.com>
2023-02-07 22:48:40 -0700

    Performed a function to delete items of a list using onDelete()

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,33 +8,61 @@
 import SwiftUI
 
 
-struct SecondView: View {
-    
-    @Environment(\.dismiss) var dismiss
-    
-    let name: String
-
-        var body: some View {
-            Button("Dismiss") {
-                dismiss()
-            }
-        }
-}
+//struct SecondView: View {
+//
+//    @Environment(\.dismiss) var dismiss
+//
+//    let name: String
+//
+//        var body: some View {
+//            Button("Dismiss") {
+//                dismiss()
+//            }
+//        }
+//}
 
 struct ContentView: View {
    
-    @State private var showingSheet = false
+//    @State private var showingSheet = false
+    
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
     
     var body: some View {
-        Button("Show Sheet") {
-        
-            showingSheet.toggle()
-        }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "@twostraws")
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add Number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .navigationTitle("onDelete()")
+            .toolbar {
+                EditButton()
+            }
+            //        Button("Show Sheet") {
+            
+            //            showingSheet.toggle()
+            // }
+            //        .sheet(isPresented: $showingSheet) {
+            //            SecondView(name: "@twostraws")
+            //        }
         }
     }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
+    }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
